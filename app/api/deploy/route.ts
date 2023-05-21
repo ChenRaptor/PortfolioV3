543467175334086b1a74d7cb7288cc2dca8ea34b
Chen <ChenRaptor@outlook.fr>
2023-05-21 11:54:38 +0200

    dashboard updated #16

--- app/api/deploy/route.ts
+++ app/api/deploy/route.ts
@@ -1,20 +1,19 @@
 import { NextResponse } from 'next/server';
  
 export async function GET(request: Request) {
-    const { exec } = require("child_process");
-    let valid = null;
-    exec("ls -la", (error: any, stdout: any, stderr: any) => {
-        if (error) {
-            const valid = `error: ${error.message}`
-            console.log(`error: ${error.message}`);
-        }
-        if (stderr) {
-            const valid = `stderr: ${stderr}`
-            console.log(`stderr: ${stderr}`);
-        }
-        const valid = `stdout: ${stdout}`
-        console.log(`stdout: ${stdout}`);
+    const execSync = require('child_process').execSync;
+    // import { execSync } from 'child_process';  // replace ^ if using ES modules
+
+    const output = execSync('ls -la', { encoding: 'utf-8' });  // the default is 'buffer'
+    const splitted = output.split(/\r?\n/);
+    const filtered = splitted.filter( (e:any) => {
+      return e !== '';
     });
+
+
+
+
+  //const product = await res.json();
  
-  return NextResponse.json({msg: valid});
+  return NextResponse.json(JSON.stringify(filtered));
 }
